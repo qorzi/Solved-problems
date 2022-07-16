79cccef0b8524c414438ada0a13440befa6f2419
haeryu <qorzi00@gmail.com>
2022-07-16 22:11:08 +0900

    bj220716

---
+++ python/BAEKJOON/bj_2588.py
@@ -0,0 +1,18 @@
+A = input()
+B = input()
+
+x = 0
+y = 0
+z = 0
+
+total = 0
+
+x = int(B[2]) * int(A)
+y = int(B[1]) * int(A)
+z = int(B[0]) * int(A)
+total = x + y * 10 + z *100
+
+print(x)
+print(y)
+print(z)
+print(total)
