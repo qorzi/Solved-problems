c02fdf203c6c06e50f81304552a6e764887f6d45
qorzi <qorzi00@gmail.com>
2022-09-27 16:56:59 +0900

    220927

--- python/SWEA/test.py
+++ python/SWEA/test.py
@@ -1,18 +1,23 @@
-arr = ['A', 'B', 'C']  # 재료 리스트
-sel = [0, 0, 0]  # 인형뽑기 selection
-check = [0, 0, 0]  # 뽑을지 말지 결정하는 리스트
+def subtree(lst):
 
 
-def perm(depth):
-    if depth == 3:
-        print(sel)
-        return
+N = int(input())
+in_lst = list(map(int, input().split()))
+post_lst = list(map(int, input().split()))
 
-    for i in range(3):
-        if not check[i] == 1:
-            check[i] = 1
-            sel[depth] = arr[i]
-            perm(depth+1)
-            check[i] = 0
+# 포스트의 마지막은 루트 값
+# 프리의 시작이 루트 값
+# 인의 중간이 루트 값
+# -> 각 서브 트리의 루트를 담으면 프리 ( 좌 -> 우 )
 
-perm(0)
+ord_lst = []
+
+root_value = post_lst[-1]
+ord_lst += [root_value]
+m = in_lst.index(root_value)
+l_tree = in_lst[:m]
+r_tree = in_lst[m+1:]
+while l_tree:
+
+    print(l_tree, root_value, r_tree)
+
