5aad42306353c2c21f06941bdf73703df14a7805
qorzi <qorzi00@gmail.com>
2022-10-14 13:02:50 +0900

    221014

--- python/SWEA/SWEA_5650/SWEA_5650.py
+++ python/SWEA/SWEA_5650/SWEA_5650.py
@@ -8,52 +8,54 @@ value4 = {(1,0):(0,-1), (-1,0):(1,0), (0,1):(-1,0), (0,-1):(0,1)}
 value5 = {(1,0):(-1,0), (-1,0):(1,0), (0,1):(0,-1), (0,-1):(0,1)}
 
 def pinball(i, j):
-    def wormhole(n, a, b):
-        for i in range(N):
-            for j in range(N):
-                if matrix[i][j] == n and (i != a and j != b):
-                    return i, j
-
     current_i , current_j = i, j
     cnt_lst = []
     for di, dj in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
         cnt = 0
         current_di, current_dj = di, dj
-        while True: # 블랙홀을 만나거나, i,j로 돌아 올때까지
-            if 0 <= current_i+di < N and 0 <= current_j+dj < N:
-                current_i += di
-                current_j += dj
-                value = matrix[current_i+current_di][current_j+current_dj]
-                print(current_i, current_j, value)
+        flag = True
+        while flag: # 블랙홀을 만나거나, i,j로 돌아 올때까지
+            if 0 <= current_i+current_di < N and 0 <= current_j+current_dj < N:
+                current_i += current_di
+                current_j += current_dj
+                # print(current_i, current_j)
+                value = matrix[current_i][current_j]
+
                 if value:
-                    print('hi')
+                    # print('value', value, cnt)
                     if 6 <= value <= 10: # 웜홀을 만났을 때
-                        current_i, current_j = wormhole(value, current_i, current_j)
+                        current_i, current_j = wormhole[current_i, current_j]
                     elif value == 1:
                         current_di, current_dj = value1[(current_di, current_dj)]
                         cnt += 1
                     elif value == 2:
-                        current_di, current_dj = value1[current_di, current_dj]
+                        current_di, current_dj = value2[current_di, current_dj]
                         cnt += 1
                     elif value == 3:
-                        current_di, current_dj = value1[current_di, current_dj]
+                        current_di, current_dj = value3[current_di, current_dj]
                         cnt += 1
                     elif value == 4:
-                        current_di, current_dj = value1[current_di, current_dj]
+                        current_di, current_dj = value4[current_di, current_dj]
                         cnt += 1
                     elif value == 5:
-                        current_di, current_dj = value1[current_di, current_dj]
+                        current_di, current_dj = value5[current_di, current_dj]
                         cnt += 1
-            else:
-                di = -di
-                dj = -dj
+            else: # 벽을 만났을 때
+                current_i += current_di
+                current_j += current_dj
+                current_di = -current_di
+                current_dj = -current_dj
                 cnt += 1
 
-            if matrix[current_i][current_j] == -1:
-                break
-            elif current_i == i and current_j == j:
-                break
+            if 0 <= current_i < N and 0 <= current_j < N:
+                if matrix[current_i][current_j] == -1:
+                    # print('end')
+                    flag = False
+                elif current_i == i and current_j == j:
+                    # print('end')
+                    flag = False
         cnt_lst.append(cnt)
+        # print(cnt_lst)
     return max(cnt_lst)
 
 
@@ -62,10 +64,27 @@ for tc in range(1, T+1):
     N = int(input())
     matrix = [list(map(int, input().split())) for _ in range(N)]
 
+    # 웜홀 찾기기
+    hole_tmp = [[] for _ in range(11)]
+    wormhole = {}
+    for i in range(N):
+        for j in range(N):
+            if 6 <= matrix[i][j] <= 10:
+                hole_tmp[matrix[i][j]].append((i,j))
+    # print(hole_tmp)
+    for idx in range(6, 11):
+        if hole_tmp[idx]:
+            # print(hole_tmp[idx][0], hole_tmp[idx][1])
+            a, b = hole_tmp[idx][0], hole_tmp[idx][1]
+            wormhole[a] = b
+            wormhole[b] = a
+    # print(wormhole)
+
     ans = 0
     for i in range(N):
         for j in range(N):
             if matrix[i][j] == 0:
+                # print('start', i, j)
                 tmp = pinball(i, j)
                 if tmp > ans:
                     ans = tmp
