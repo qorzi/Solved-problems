99e36f1002006a0426244f833b1d72910523513a
qorzi <qorzi00@gmail.com>
2023-05-17 09:07:41 +0900

    programmers 문자열 교환

--- BAEKJOON/1522_문자열 교환/1522_문자열 교환.py
+++ BAEKJOON/1522_문자열 교환/1522_문자열 교환.py
@@ -1,18 +1,18 @@
-# def min_swaps(word, a, b):
-#     num_a = word.count(a)
-#     num_b = word[:num_a].count(b)
-#     min_b = num_b
+def min_swaps(word, a, b):
+    num_a = word.count(a)
+    num_b = word[:num_a].count(b)
+    min_b = num_b
 
-#     for i in range(len(word)-num_a):
-#         num_b = word[i:num_a+1].count(b)
-#         if min_b < num_b:
-#             min_b = num_b
+    for i in range(len(word)-num_a):
+        num_b = word[i:num_a+1].count(b)
+        if min_b < num_b:
+            min_b = num_b
 
-#     return min_b
+    return min_b
 
 
-# word = str(input())
-# print(min(min_swaps(word, 'a', 'b'), min_swaps(word, 'b', 'a')))
+word = str(input())
+print(min(min_swaps(word, 'a', 'b'), min_swaps(word, 'b', 'a')))
 
 
 def min_swaps(word, a, b):
